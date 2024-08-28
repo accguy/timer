c4a89ea135cfb0b10de26431b83fb12c83be2cef
accguy <accguy2190@naver.com>
2024-08-28 15:41:15 +0900

    시작 리셋 기능 구현

---
+++ index.js
@@ -0,0 +1,53 @@
+const $hrsInput = document.querySelector(".hrs-input");
+const $minInput = document.querySelector(".min-input");
+const $secInput = document.querySelector(".sec-input");
+const $startBtn = document.querySelector(".start-btn");
+const $resetBtn = document.querySelector(".reset-btn");
+
+const $hrsDisplay = document.querySelector(".hrs-display");
+const $minDisplay = document.querySelector(".min-display");
+const $secDisplay = document.querySelector(".sec-display");
+
+let isRunning = false;
+
+$startBtn.addEventListener("click", () => {
+  if (!isRunning) {
+    isRunning = true;
+
+    let realTime =
+      +$hrsInput.value * 3600 + +$minInput.value * 60 + +$secInput.value;
+
+    const timer = setInterval(() => {
+      realTime -= 1;
+
+      if (realTime == 0) {
+        window.alert("Finish");
+        clearInterval(timer);
+      }
+      if ($hrsDisplay.value == 0) {
+        $hrsDisplay.value = 0;
+      }
+      if ($minDisplay.value == -1) {
+        $minDisplay.value = 59;
+      }
+      if ($secDisplay.value == -1) {
+        $secDisplay.value = 59;
+      }
+      $hrsDisplay.value = Math.floor(realTime / 3600);
+      $minDisplay.value = Math.floor((realTime % 3600) / 60);
+      $secDisplay.value = realTime % 60;
+    }, 1000);
+
+    $resetBtn.addEventListener("click", () => {
+      if (timer) {
+        clearInterval(timer);
+      }
+      $hrsInput.value = 0;
+      $minInput.value = 0;
+      $secInput.value = 0;
+      $hrsDisplay.value = 0;
+      $minDisplay.value = 0;
+      $secDisplay.value = 0;
+    });
+  }
+});
